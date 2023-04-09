89f2e8d7854879bbd6bf862404c2cde5cfff59e8
Joy Johns <JRJ20716@gmail.com>
2023-04-09 16:13:37 -0400

    Delete lastAccessed & added thoughts & friends

--- models/User.js
+++ models/User.js
@@ -6,10 +6,11 @@ const userSchema = new mongoose.Schema({
   // Add individual properties and their types
   username: { type: String, unique: true, required: true, trimmed: true },
   email: { type: String, required: true, unique: true, validate: [validateEmail], match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]},
-  // https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+    // https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+
+  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   },
-  // Use built in date method to get current date
-  lastAccessed, { type: Date, default: Date.now },
 );
 // 'User' is the name of the model
 // userSchema is the name of the schema we are using to create a new instance of the model
