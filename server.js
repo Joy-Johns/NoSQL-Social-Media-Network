9d9c61dd3170f4c4f695e2ef9978ee14d88fe598
Joy-Johns <jrj20716@gmail.com>
2023-05-08 19:04:09 -0400

    add moment packet and correct server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const { User } = require('./models');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
