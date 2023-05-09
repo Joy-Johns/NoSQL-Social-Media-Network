9f0dd1e136da08e5edbf10b7dda3e837b960edd1
Joy-Johns <jrj20716@gmail.com>
2023-05-08 20:07:26 -0400

    delete and get by id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -40,6 +40,42 @@ const thoughtController = {
 
     },
 
+    //   get_a_Thought_ById
+    get_a_Thought_ById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .then((dbThoughtData) => {
+            // if no thought is found
+            if (!dbThoughtData) {
+            res.status(404).json({ message: "No thought with this ID" });
+            return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // delete a thought
+    delete_the_Thought(req, res) {
+        Thought.findOneAndDelete({_id: req.params.id})
+        .then((thought) => {
+            if(!thought){
+                res.status(404).json({message: 'No thought with that ID'}) 
+
+
+            }      
+            
+            return User.findOneAndUpdate(
+                {_id:req.body.userID},
+                {$pull:{thoughts:thought._id}},
+                {new:true}
+    
+            )
+    }).then(() => res.json({message: 'though deleted!'})).catch((err) => res.status(500).json(err));
+    },
+    
     // add Reaction
     add_new_Reaction(req, res) {
         console.log('You are adding a reaction');
@@ -57,10 +93,26 @@ const thoughtController = {
             : res.json(thought)
         )
         .catch((err) => res.status(500).json(err));
+    },
+
+    //delete Reaction
+    delete_the_Reaction(req, res) {
+    console.log(req.params)
+
+        Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId} } },
+        { runValidators: true, new: true }
+        )
+        .then((thought) =>
+            !thought
+            ? res
+                .status(404)
+                .json({ message: 'No thought found with that ID' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
     }
-
-
-
 
 }
 
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -33,6 +33,23 @@ const userController = {
 
     },
 
+
+
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.id })
+        .then((user) => !user ? res.status(404).json({ message: 'No user with that ID' }) : Thought.deleteMany({
+            _id: {
+                $in: user.thoughts
+            }
+        })).then(() => res.json({ message: 'User and associated apps deleted!' })).catch((err) => res.status(500).json(err));
+    },
+
+    // getUserById,
+    getUserById(req, res) {
+        User.findOne({ _id: req.params.id })
+        .then((user) => !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
     // add a new Friend
     addFriend(req, res) {
         console.log("addFriend got call from user controller")
@@ -41,6 +58,21 @@ const userController = {
             _id: req.params.id
         }, {
             $addToSet: {
+                friends: req.params.friendsId
+            }
+        }, {
+            runValidators: true,
+            new: true
+        })
+        .then((user) => !user ? res.status(404).json({ message: 'No friend with id ' + req.params.friendsId + ' does not exist' }) : res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
+    // remove the Friend selected
+    removeFriend(req, res) {
+        User.findOneAndUpdate({
+            _id: req.params.id
+        }, {
+            $pull: {
                 friends: req.params.friendsId
             }
         }, {
